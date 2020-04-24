d3acb276672e52daa8c2a0640c00090b2b92d606
Rapunzel <Laurenminton19@yahoo.com>
2020-04-24 15:08:48 -0400

    cant get notes to save

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const fs = require("fs");
 const path = require("path");
 
-const notes = require("./db/db.json");
+const notes = require("./db/notes");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
