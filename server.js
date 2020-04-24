f733076dba7ffdbb41b60411a32db13831ecba4b
Rapunzel <Laurenminton19@yahoo.com>
2020-04-24 14:50:37 -0400

    fixed bugs added routes

--- server.js
+++ server.js
@@ -1,14 +1,67 @@
+
 const express = require("express");
-const htmlRoutes = require("./routes/htmlRoutes");
-const apiRoutes = require("./routes/apiRoutes");
+const fs = require("fs");
+const path = require("path");
+
+const notes = require("./db/db.json");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static("public"));
-app.use("/", htmlRoutes);
-app.use("/api", apiRoutes);
+app.use(express.static('public'));
 
-app.listen(PORT, () => console.log("Connected"));
+
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
+  
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    return res.json(notes);
+});
+app.post("/api/notes", function(req, res) {
+    const newNote = req.body;
+
+    if (notes.length <= 0) {
+        const noteID = 1;
+        newNote.id = noteID;
+    }
+    else {
+        const i = notes.length - 1;
+        const noteID = notes[i].id + 1;
+        newNote.id = noteID;
+    }
+
+    notes.push(newNote);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), err => {
+        if (err) throw err;
+        res.json(newNote);
+    });
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    const deleteID = parseInt(req.params.id);
+    for (let i = 0; i < notes.length; i++) {
+        if (deleteID === notes[i].id) {
+            console.log("Deleted selected note from json.db");
+            notes.splice(i, 1);
+        }
+    }
+
+    fs.writeFile("./db/db.json", JSON.stringify(notes), err => {
+        if (err) throw err;
+        res.json(notes);
+    });
+});
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});
